4a5ed1ff1c01200e647d5d3198bdd1420515aebe
mariannacoppa <densimetra@gmail.com>
2024-09-05 10:17:34 +0200

    [UPD] added string lengths and comments

--- welcome.php
+++ welcome.php
@@ -10,7 +10,21 @@
     <title>Document</title>
 </head>
 <body>
-    <!-- Welcome <?php // echo $nome." ".$cognome; ?> in php world -->
-    Welcome <?php echo $nome." ".str_replace('cognome', '***', 'cognome'); ?> in php world <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="content">
+                    <!-- stampo i nomi inseriti nel form -->
+                    <h1>Welcome <?php echo $nome." ".$cognome; ?> in php world</h1>
+                    <!-- censuro la seconda parola inserita nel form -->
+                    <p>Welcome <?php echo $nome." ".str_replace('cognome', '***', 'cognome'); ?> in php world</p>
+                    <!-- stampo la lunghezza dei caratteri totali inseriti nel form -->
+                    <p>Somma di nome e cognome: <?php echo strlen($nome.$cognome); ?> caratteri</p>
+                    <!-- Somma dei termini con il termine censurato -->
+                     <p>Somma di nome e cognome censurato: <?php echo strlen($nome.'***'); ?> </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
